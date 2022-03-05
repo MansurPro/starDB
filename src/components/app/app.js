bef32521f5c3e26cb1568f4b86e17cc76d4d9efc
Mansur <60543298+MansurPro@users.noreply.github.com>
2022-03-05 15:44:20 +0600

    Update app.js

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -5,6 +5,7 @@ import RandomPlanet from '../random-planet';
 import {PeoplePage, PlanetPage, StarshipPage} from '../pages';
 import {Provider} from '../swapi-context';
 import SwapiService from '../../services/swapi-service';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import './app.css';
 
 const swapi = new SwapiService();
@@ -13,9 +14,28 @@ const App = () => {
   return (
     <div>
       <Provider value={swapi}>
-        <Header />
-        <RandomPlanet />
-        <PeoplePage />
+        <Router>
+          <Header />
+          <RandomPlanet />
+          <Route path='/' exact
+            render={() => (
+              <div id='cont'>
+                <h1>Welcome to Star Wars</h1>
+              </div>
+            )} />
+          <Route path='/people/:id?' render={({ match }) => {
+            const selectedItemId = match.url === '/people/' ? 1 : match.params.id
+            return <PeoplePage selectedItemId={selectedItemId}/>
+          }}/>
+          <Route path='/planet/:id?' render={({ match }) => {
+            const selectedItemId = match.url === '/planet/' ? 2 : match.params.id
+            return <PlanetPage selectedItemId={selectedItemId}/>
+          }}/>
+          <Route path='/starship/:id?' render={({ match }) => {
+            const selectedItemId = match.url === '/starship/' ? 5 : match.params.id
+            return <StarshipPage selectedItemId={selectedItemId}/>
+          }}/>
+        </Router>
       </Provider>
     </div>
   );
