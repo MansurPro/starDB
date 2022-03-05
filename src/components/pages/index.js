3a58f2be8c1e0d6374e3fd02be51afe7ec764f2b
Mansur <60543298+MansurPro@users.noreply.github.com>
2022-03-05 15:49:06 +0600

    Update index.js

--- src/components/pages/index.js
+++ src/components/pages/index.js
@@ -1,5 +1,5 @@
-import { PeoplePage } from './people-page';
-import { PlanetPage } from './planet-page';
-import { StarshipPage } from './starship-page';
+import PeoplePage from './people-page';
+import PlanetPage from './planet-page';
+import StarshipPage from './starship-page';
 
 export { PlanetPage, PeoplePage, StarshipPage };
